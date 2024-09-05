e438f844b83027672e455293bb3f80990c81ba6f
Weijt133 <weijt133@gmail.com>
2024-09-05 23:55:00 +1000

    Initial commit

---
+++ src/main/java/com/assessment/YOUx/handler/MyLambdaHandler.java
@@ -0,0 +1,7 @@
+package com.assessment.YOUx.handler;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class MyLambdaHandler extends SpringBootRequestHandler<String, String> {
+    
+}
